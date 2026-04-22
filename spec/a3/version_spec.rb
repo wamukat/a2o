21eff78b672eb6adb1d14d24a585d9682c0b25ea
wamukat <takuma.watanabe@gmail.com>
2026-04-22 17:10:21 +0900

    A2O#143 Release v0.5.11

--- spec/a3/version_spec.rb
+++ spec/a3/version_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 RSpec.describe "A3 version" do
-  it "matches the A2O 0.5.10 release version" do
-    expect(A3::VERSION).to eq("0.5.10")
+  it "matches the A2O 0.5.11 release version" do
+    expect(A3::VERSION).to eq("0.5.11")
   end
 end
