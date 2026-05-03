c2fba512f4f08642af0726a04b9b4415ca522057
wamukat <takuma.watanabe@gmail.com>
2026-05-03 09:30:12 +0900

    A2O#535 Prepare v0.5.64 release

--- spec/a3/version_spec.rb
+++ spec/a3/version_spec.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 RSpec.describe "A3 version" do
-  it "matches the A2O 0.5.63 release version" do
-    expect(A3::VERSION).to eq("0.5.63")
+  it "matches the A2O 0.5.64 release version" do
+    expect(A3::VERSION).to eq("0.5.64")
   end
 
   it "keeps release workflow version aligned" do
